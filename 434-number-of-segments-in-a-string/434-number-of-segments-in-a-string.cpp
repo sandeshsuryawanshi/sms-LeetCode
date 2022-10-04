4a604f8e9871f7bba3c70eb59adecec952123948
Sandesh Dilip Suryawanshi <107345943+sandeshsuryawanshi@users.noreply.github.com>
2022-10-05 01:01:53 +0530

    Time: 3 ms (36.47%), Space: 6.2 MB (9.86%) - LeetHub

---
+++ 434-number-of-segments-in-a-string/434-number-of-segments-in-a-string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int countSegments(string s) 
+    {
+        
+         int word = 0;
+    for(int i = 0; i < s.size(); i ++){
+        if (s[i] != ' ' && s[i+1] == ' ' || s[i] != ' ' && s[i+1] == '\0'){
+            word++;
+        }
+    } 
+    return word;
+    }
+};
