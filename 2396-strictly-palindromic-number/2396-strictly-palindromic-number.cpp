1d990e31fc8b1df2a34db2f83f7e8110a31c598f
Sandesh Dilip Suryawanshi <107345943+sandeshsuryawanshi@users.noreply.github.com>
2023-01-16 03:12:58 +0530

    Time: 3 ms (39.55%), Space: 6.2 MB (16.64%) - LeetHub

---
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    
+     int Base(int i,int n)
+     {
+         vector<int>v;
+         while(n>0)    
+           {    
+            int k=n%i; 
+             v.push_back(k);
+            n=n/2;    
+            } 
+         int s=0,e=v.size()-1;
+         
+           while(s<=e)
+           {
+               if(v[s]!=v[e])
+               {
+                   return 0;
+               }
+               s++;
+               e--;
+           }
+         return 1;
+         
+     }
+    bool isStrictlyPalindromic(int n)
+    
+    {
+        
+        for(int i=2;i<=n-2;i++)
+        {
+            int p= Base(i,n);
+            if(p==0)
+            {
+                return false;
+            }
+           
+        }
+        return true;
+        
+    }
+};
