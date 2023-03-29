4efe526958f7a79b36d9efb3ae7d3d5d5a4169ed
Sandesh Dilip Suryawanshi <107345943+sandeshsuryawanshi@users.noreply.github.com>
2023-03-29 10:37:07 +0530

    Time: 61 ms (100.00%), Space: 51.2 MB (76.56%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.ts
+++ 2540-minimum-common-value/2540-minimum-common-value.ts
@@ -1,33 +1,10 @@
-var getCommon = function(nums1, nums2) {
-  
+function getCommon(nums1: number[], nums2: number[]): number {
+    let i = 0, j = 0
 
-let map = {};
+    while (i < nums1.length && j < nums2.length) {
+        if (nums1[i] === nums2[j]) return nums1[i]
+        nums1[i] < nums2[j] ? i++ : j++
+    }
 
-        for (let i = 0 ; i < nums1.length ; i++)
-            {
-                const item = nums1[i];
-
-                if(!map[item])
-                {
-                    map[item] = 1;
-                }
-                else 
-                {
-                    map[item] = map[item]+1;
-                }
-            }
-
-
-        for(let j = 0 ; j < nums2.length ; j ++)
-            {
-                const item2 = nums2[j];
-
-                if(map[item2])
-                {
-                    return item2;
-                }
-            }
-
-        return -1;
-    
+    return -1
 };
