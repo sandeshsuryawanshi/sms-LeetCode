849ad540cdf7c5e4992716017429badbf31b4b34
Sandesh Dilip Suryawanshi <107345943+sandeshsuryawanshi@users.noreply.github.com>
2023-03-29 10:23:31 +0530

    Time: 71 ms (41.19%), Space: 44.7 MB (17.36%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.js
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.js
@@ -1,23 +1,19 @@
-class Solution {
-public:
-    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) 
-    {
-          map<int,int>mp;
-        for(int i=0;i<nums1.size();i++)
-        {
-            mp[nums1[i]]++;
+var intersect = function(nums1, nums2) {
+    let map = new Map();
+    let res = [];
+    for(let i = 0 ; i < nums1.length ; i++) {
+        if(map.has(nums1[i])) {
+            map.set(nums1[i],map.get(nums1[i])+1);
         }
-        vector<int>v;
-        for(int i=0;i<nums2.size();i++)
-        {
-            if(mp[nums2[i]]!=0)
-            {
-                v.push_back(nums2[i]);
-                   mp[nums2[i]]--;
-            }
-         
+        else{
+            map.set(nums1[i],1);
         }
-       
-         return v;
     }
+    for(let i = 0 ; i < nums2.length ; i++) {
+            if(map.get(nums2[i])>0) {
+                res.push(nums2[i])
+                map.set(nums2[i],map.get(nums2[i])-1);
+            }
+    }
+    return res;
 };
