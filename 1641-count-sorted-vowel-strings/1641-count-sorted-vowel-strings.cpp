641131c7f9099b738a7c9ec37ba4178757435fcc
Sandesh Dilip Suryawanshi <107345943+sandeshsuryawanshi@users.noreply.github.com>
2023-04-05 23:23:16 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (74.34%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
@@ -1,22 +1,42 @@
 class Solution {
 public:
+     int dp[51][51];
+     int solve(int n,int i)
+     {
+         if(n==0)
+         {
+             return 1;
+         }
+         if(dp[n][i]!=-1)
+         {
+             return dp[n][i];
+         }
+         if(i>=5)
+         {
+             return 0;
+         }
+         return dp[n][i]= solve(n-1,i)+solve(n,i+1);
+     }
     int countVowelStrings(int n) 
     {
-        vector<int>ans(5,1);  // 5 size of array ans assign value for each index from 0 to 5-1
+//         vector<int>ans(5,1);  // 5 size of array ans assign value for each index from 0 to 5-1
         
-        for(int i=2;i<=n;i++)
-        {
-            for(int j=3;j>=0;j--)
-            {
-                ans[j]+=ans[j+1];
+//         for(int i=2;i<=n;i++)
+//         {
+//             for(int j=3;j>=0;j--)
+//             {
+//                 ans[j]+=ans[j+1];
                 
-            }
-        }
+//             }
+//         }
         
-        return accumulate(ans.begin(),ans.end(),0);
+//         return accumulate(ans.begin(),ans.end(),0); // it can calculate the sum from 0 to 5-1 of the vector
         
+         
         
-        
+        memset(dp,-1,sizeof(dp));
+       
+    return solve(n,0);
         
     }
 };
