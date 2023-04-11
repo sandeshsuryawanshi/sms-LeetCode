e312a25a086091221804c8880fa7c3b1ad566a7c
Sandesh Dilip Suryawanshi <107345943+sandeshsuryawanshi@users.noreply.github.com>
2023-04-11 17:01:00 +0530

    Time: 199 ms (5.04%), Space: 44.7 MB (5.04%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -3,24 +3,50 @@ public:
     string removeStars(string s) 
     {
     
-        
+        stack<int>st;
          string str="";
         int counter=0;
-        for(int i=s.size()-1;i>=0;i--)
-        {
-            if(s[i]=='*')
-            {
-                counter++;
-            }
-            else if(counter==0)
-            {
-                str+=s[i];
-            }
-            else
-            {
-                counter--;
-            }
-        }
+        // for(int i=s.size()-1;i>=0;i--)
+        // {
+        //     if(s[i]=='*')
+        //     {
+        //         counter++;
+        //     }
+        //     else if(counter==0)
+        //     {
+        //         str+=s[i];
+        //     }
+        //     else
+        //     {
+        //         counter--;
+        //     }
+        // }
+        
+           for(int i=0;i<s.size();i++)
+           {
+               st.push(s[i]);
+           }
+           while (!st.empty())
+           {
+               char ch=st.top();
+               if(ch=='*')
+               {
+                   counter++;
+                   st.pop();
+               }
+               else if(counter==0)
+               {
+                   str+=ch;
+                   st.pop();
+               }
+               else
+               {
+                   counter--;
+                   st.pop();
+               }
+               
+           }
+           
         reverse(str.begin(),str.end());
         return str;
     }
